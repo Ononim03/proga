884ae0182f10feb0c086872fde3cfd71dbeca564
Ononim03 <93390852+Ononim03@users.noreply.github.com>
2025-03-12 09:58:52 +0300

    Add files via upload

---
+++ tech-practice.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+struct Abiturient {
+	string name;
+	string birth;
+	int score_first;
+	int score_second;
+	int score_third;
+	int	sum_score;
+};
+int fill_arrray(Abiturient* arr) {
+	ifstream in;
+	in.open("in.txt");
+	string part_name;
+	string birth;
+
+	Abiturient ab;
+
+	int index = 0;
+	while (in >> part_name) {
+		ab.name = part_name;
+		in >> part_name;
+		ab.name += " " + part_name;
+		in >> part_name;
+		ab.name += " " + part_name;
+		in >> ab.birth;
+		in >> ab.score_first;
+		in >> ab.score_second;
+		in >> ab.score_third;
+		ab.sum_score = ab.score_third + ab.score_first + ab.score_second;
+		arr[index] = ab;
+		index++;
+
+	}
+	in.close();
+	return index;
+}
+void sort_abiturients(Abiturient* arr, int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (arr[j].sum_score < arr[j + 1].sum_score) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+void print_scores(Abiturient* arr, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i].sum_score << endl;
+	}
+}
+void output_students(Abiturient* arr) {
+	int count;
+	ofstream out;
+	out.open("out.txt");
+	cout << "Enter count: ";
+	cin >> count;
+	int last_score;
+	int index = 0;
+	while (index < count) {
+		out << arr[index].name + " " + arr[index].birth + " " << arr[index].sum_score << endl;
+		index++;
+	}
+	last_score = arr[index].sum_score;
+	out.close();
+	if (last_score == arr[index + 1].sum_score) {
+		cout << "extra list is created";
+		ofstream extraout;
+		extraout.open("extra.txt");
+		while (last_score == arr[index + 1].sum_score) {
+			extraout << arr[index].name + " " + arr[index].birth + " " << arr[index].sum_score << endl;
+			index++;
+		}
+		cout << arr[index + 1].sum_score;
+		extraout.close();
+	}
+	
+}
+int main()
+{
+	int n;
+	Abiturient* arr = new Abiturient[1000000];
+	n = fill_arrray(arr);
+	sort_abiturients(arr, n);
+	output_students(arr);
+}
